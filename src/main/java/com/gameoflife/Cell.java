fd7c101ddc21917751fc1adc4b34e4029b3b6462
Andrew Snead <andrewcsnead@gmail.com>
2015-03-08 15:04:00 +0000

    setting up universe class

--- src/main/java/com/gameoflife/Cell.java
+++ src/main/java/com/gameoflife/Cell.java
@@ -13,6 +13,10 @@ public class Cell {
   }
 
   public CellState getNextState(int i) {
-    return i > 1  && i < 4 ? CellState.ALIVE : CellState.DEAD;
+    if (state == CellState.ALIVE) {
+      return i > 1  && i < 4 ? CellState.ALIVE : CellState.DEAD;
+    } else {
+      return i == 3 ? CellState.ALIVE : CellState.DEAD;
+    }
   }
 }
---
+++ src/test/java/com/gameoflife/UniverseTest.java
@@ -0,0 +1,14 @@
+package gameoflife;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class UniverseTest {
+
+  @Test
+  public void storesItsInitialState() {
+    new Universe();
+  }
+
+}
